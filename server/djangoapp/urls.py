793f9dbef15ff8628591569b8645605fdba9d555
Hardwork <hardworking8787@gmail.com>
2025-06-13 20:11:36 +0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,28 +6,13 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
-    # Path for registration
     path('register/', views.registration, name='register'),
-
-    # Path for login
     path(route='login', view=views.login_user, name='login'),
-
-    # Path for logout
     path('logout/', views.logout_user, name='logout'),
-
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    # path('dealer-reviews/', views.get_dealer_reviews, name='dealer_reviews'),
-
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-
     path(route='add_review', view=views.add_review, name='add_review'),
-
-    # Path for adding a review (future implementation)
-    # path('add-review/', views.add_review, name='add_review'),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
