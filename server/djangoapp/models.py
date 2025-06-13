8289159c0542121bc9e61d05defcff0ff94a2144
Hardwork <hardworking8787@gmail.com>
2025-06-13 20:03:40 +0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,7 @@
 # Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
 
 # Create your models here.
 
@@ -15,7 +13,6 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
